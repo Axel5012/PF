42b8b21358afb3382301628bc8fc918367f577fe
Axel5012 <85588742+Axel5012@users.noreply.github.com>
2021-06-08 19:31:24 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,10 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyB9bM5oPUrdVExRk9CubxcOW9fLbc62wXU",
-    authDomain: "bdfstorage50.firebaseapp.com",
-    projectId: "bdfstorage50",
-    storageBucket: "bdfstorage50.appspot.com",
-    messagingSenderId: "664107957967",
-    appId: "1:664107957967:web:143655f3e3bac9c35001ba",
-    measurementId: "G-34XBMLR25H"
+  piKey: "AIzaSyCr-6Rbvsp7vM65boRcTxQUvcE8PlwC6cA",
+    authDomain: "pf3cv50.firebaseapp.com",
+    projectId: "pf3cv50",
+    storageBucket: "pf3cv50.appspot.com",
+    messagingSenderId: "1098534119081",
+    appId: "1:1098534119081:web:f570023d3ee7c105b66bb8"
 });
